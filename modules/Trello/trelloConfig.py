61a98e766a293a2024028d0847fe19e7af86f97f
Brian Gabriel Díaz Rodríguez <briangdr.22@gmail.com>
2025-07-12 02:16:25 -0600

    Se movieron las funciones de la api de trello a otro archivo

---
+++ modules/Trello/trelloConfig.py
@@ -0,0 +1,21 @@
+#Trello API constants
+TRELLO_ENDPOINT = "https://api.trello.com/1/cards"
+TRELLO_HEADERS = {"Accept": "application/json"}
+TRELLO_TOKEN="ATTA3bb61958d5b3506b54860df6410561d10f408ed793e46cc60846737fac06cbc07E9CEBB9"
+TRELLO_KEY="2c0369eb0c76fbbbf4ecf3094fd31356"
+TRELLO_ID_LIST="66db40b0006d1f0947656dec"
+
+
+labels = {
+    "Garantía": "66db3f8a10ea602ee6292ec5",
+    "Consulta": "66db3f8a10ea602ee6292ec2",
+    "Reparación": "66db3f8a10ea602ee6292ec3",
+    "Soporte": "66db3f8a10ea602ee6292eca",
+}
+
+members = {
+    "GABRIEL": "64ece20aae1eb29dbbdeae66",
+    "CENTRO SERVICIO": "613199d8efadf1307693adda",
+    "CORAL": "6446fd35f1f734d3ec6183bd",
+    "DAMARIS": '662fc3c7444178dbf67e6d27'
+}
